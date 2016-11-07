6b048730ec56785ea3ce2624252bedf1ba793e66
Nadeem Haidar <nadeem.haidar@mail.utoronto.ca>
2016-11-06 22:12:02 -0500

    Added animated lines within progress bar

--- YouTube15/YouTube15.cs
+++ YouTube15/YouTube15.cs
@@ -188,6 +188,7 @@ namespace YouTube15
         }
 
         private Byte[] emptyBg = new Byte[LogiLcd.MonoWidth * LogiLcd.MonoHeight];
+        private int lineTrack = 4;
 
         public void UpdateLcd()
         {
@@ -229,8 +230,17 @@ namespace YouTube15
                     DrawTextScroll(g, 1, api.getUploader());
                     DrawTextScroll(g, 3, String.Format("{0}:{1:D2} / {2}:{3:D2}", pos / 60, pos % 60, len / 60, len % 60));
 
+                    // draw progress bar
                     g.DrawRectangle(Pens.White, 3, 24, LogiLcd.MonoWidth - 6, 4);
                     g.FillRectangle(Brushes.White, 3, 24, (int)((LogiLcd.MonoWidth - 6) * perc), 4);
+
+                    // draw animated lines within progress bar
+                    if (lineTrack > 8)
+                        lineTrack = 4;
+                    else
+                        lineTrack++;
+                    for (int x = lineTrack; x < LogiLcd.MonoWidth - 6; x += 6)
+                        g.DrawLine(Pens.Black, new Point(x, 26), new Point(x + 2, 26));
 
                     if (api.playing())
                     {
